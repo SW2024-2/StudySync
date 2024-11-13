55d1b2ca267ebfefd958e31106edb3be2ef23a00
Ubuntu <2211140043v@ed.fuk.kindai.ac.jp>
2024-11-13 09:00:01 +0000

    ログイン用のコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'reports/index'
   get 'reports/show'
   get 'comments/create'
   get 'comments/destroy'
   get 'likes/create'
   get 'likes/destroy'
-=======
+  
   get 'friendships/create'
   get 'friendships/destroy'
   get 'study_logs/index'
@@ -22,7 +21,7 @@ Rails.application.routes.draw do
   get 'users/edit'
   get 'users/update'
   get 'users/destroy'
->>>>>>> model_and_controller
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
