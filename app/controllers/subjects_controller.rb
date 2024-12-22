ce118cc2366425e211abab1c6eaa947d1bc25dba
Ubuntu <2211140043v@ed.fuk.kindai.ac.jp>
2024-12-22 15:58:44 +0000

    科目設定後に元のページに戻れるようにした

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,15 +1,16 @@
+# app/controllers/subjects_controller.rb
 class SubjectsController < ApplicationController
   def new
+    session[:return_to] = request.referer
     @subject = Subject.new
   end
 
   def create
     @subject = Subject.new(subject_params)
     if @subject.save
-      # リファラ（戻る元のURL）にリダイレクト
-      redirect_to params[:return_to] || study_logs_path, notice: "科目が登録されました。"
+      redirect_to session.delete(:return_to) || root_path, notice: '科目が登録されました。'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
