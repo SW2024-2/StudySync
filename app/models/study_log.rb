7959ee229073cd703ca6526caae0720f6670c83a
Ubuntu <2211140043v@ed.fuk.kindai.ac.jp>
2024-12-22 08:06:56 +0000

    学習記録画面のエラー表示を修正

--- app/models/study_log.rb
+++ app/models/study_log.rb
@@ -3,11 +3,6 @@ class StudyLog < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-# 学習日をフォーマットして返す
-def formatted_study_date(format = '%Y/%m/%d')
-  created_at.strftime(format)
-end
-
 
   # 学習日をフォーマットして返す
   def formatted_study_date(format = '%Y/%m/%d')
@@ -68,8 +63,36 @@ end
   def liked_by?(user)
     likes.exists?(user: user)
   end
-  
-    # 学習時間が1分以上であることを検証
-  validates :study_time, numericality: { greater_than_or_equal_to: 1, message: '学習時間は1分以上である必要があります' }
-  
+
+  # 学習時間が1分以上であることを検証
+  validate :study_time_is_valid
+
+  # 学習時間の記録方法が未選択の場合にエラーメッセージを追加
+  validate :study_time_method_selected
+
+  # タイトルのバリデーション（カスタム）
+  validate :custom_validations
+
+  def custom_validations
+    if study_time.blank?
+      errors.add(:base, '学習時間を入力してください')
+    end
+    if subject.blank?
+      errors.add(:base, '教科を入力してください')
+    end
+  end
+
+  # 学習時間の記録方法が選択されていない場合にエラーメッセージを追加
+  def study_time_method_selected
+    if study_time_method.blank?
+      errors.add(:base, '学習時間の記録方法を選択してください')
+    end
+  end
+
+  # 学習時間が1分以上であることを検証
+  def study_time_is_valid
+    if study_time.present? && study_time < 1
+      errors.add(:base, '学習時間を1分以上である必要があります')
+    end
+  end
 end
